fcc1979eb78f05458b819aecad7769881fdcacae
Ubuntu <ubuntu@ip-172-31-20-198.us-west-2.compute.internal>
2013-07-07 17:12:04 +0000

    Modified buffer encoding

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs'); 
   var buf = new Buffer(256);
-  response.send(buf.toString('utc-8', fs.readFileSync('index.html')));
+  response.send(buf.toString('utc 8', fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
